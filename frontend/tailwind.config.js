7a4f921de4fb127b1a0afb2baf7130ea8de4bec2
kenagt <begic_kenan@hotmail.com>
2025-03-21 14:19:21 +0100

    - Fixing path

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./frontend/**/*.{js,ts,jsx,tsx}",
   ],
   darkMode: 'class', // Enable dark mode with class strategy
   theme: {
